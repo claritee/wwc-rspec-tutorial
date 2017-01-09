b0b518db4e52adcf26b0863eecb3135c7921d674
cltran <cltran@gumtree.com>
2017-01-09 12:24:25 +0000

    Updating test descriptions to be clearer

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,10 +2,10 @@ require_relative '../calculator'
 
 describe Calculator do
   describe "add" do
-    it "numbers" do
+    it "returns 3 adding 1 and 2" do
       expect(Calculator.add(1, 2)).to eq(3)
     end
-    it "negative numbers" do
+    it "returns the correct number when adding a negative number" do
       expect(Calculator.add(1, -2)).to eq(-1)
     end
   end
